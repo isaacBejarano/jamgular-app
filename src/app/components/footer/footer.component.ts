6c07ce0d5cdbbb6692c075e77d9bc95009cfa7c1
isaacBejarano <isaacbejaranopro@gmail.com>
2021-10-13 00:22:29 +0200

    img sizes

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -9,15 +9,28 @@ export class FooterComponent {
   footer = {
     copy: 'Copyright © 2021-2022 Isaac Bejarano',
     made: [
-      ['Angular', './assets/icons/angular.png', 'https://angular.io'],
-      ['Scully', './assets/icons/scully.png', 'https://scully.io/'],
+      [
+        'Angular',
+        './assets/icons/angular_sm_@2x.png',
+        'https://angular.io',
+        'logo de angular',
+      ],
+      [
+        'Scully',
+        './assets/icons/scully_sm_@2x.png',
+        'https://scully.io/',
+        'logo de scully',
+      ],
     ],
     powered: [
       'Netlify',
-      './assets/icons/netlify.png',
+      './assets/icons/netlify_sm_@2x.png',
       'https://www.netlify.com',
+      'logo de netlify',
     ],
-    badge:
+    badge: [
       'https://api.netlify.com/api/v1/badges/425f70ba-312a-4bbc-8ffd-387ad037e00a/deploy-status',
+      'deploy status on netlify cdn',
+    ],
   };
 }
